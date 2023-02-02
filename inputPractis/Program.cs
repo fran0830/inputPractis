045e7d3e431f69ba416bb130b5832c7add5639ff
fran0830 <obsidian4423@outlook.jp>
2023-02-02 21:29:34 +0900

    自分なりに改竄した結果 眠いから寝る。

--- inputPractis/Program.cs
+++ inputPractis/Program.cs
@@ -7,16 +7,13 @@ class Programm
 {
     static void Main()
     {
-        //入力を促すメッセージの表示をして、文字入力してもらう。
-        Console.Write("あなたの名前はなんですか？:  ");
+        //自身の名前を表示する。
+        Console.Write("私の名前は猫です。");
+
+        //外部からの入力を受け付ける。
+        Console.Write("あなたの名前は？");
         var name = Console.ReadLine();
-
-        //入力を促すメッセージの表示を行い、数値を入力してもらう
-        Console.Write("あなたの年齢を入力して下さい。:  ");
-        var age = int.Parse(Console.ReadLine());
-
-        //メッセージの出力
-        Console.WriteLine("{0}({1}歳) さん、ようこそお越しくださいました。", name, age);
+        Console.WriteLine("{0}です", name);
     }
 }
 
